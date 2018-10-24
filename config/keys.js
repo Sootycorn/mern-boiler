97a8eb3ff4e892d741660db793622d26f494d0d6
Axel Gyllensvaan <axel.gyllensvaan@renzgroup.se>
2018-10-24 16:24:30 +0200

    First Commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI1: 'mongodb://Sootycorn:Mohabgyl12#@sooty-shard-00-00-izbgc.mongodb.net:27017,sooty-shard-00-01-izbgc.mongodb.net:27017,sooty-shard-00-02-izbgc.mongodb.net:27017/test?ssl=true&replicaSet=Sooty-shard-0&authSource=admin&retryWrites=true',
+    mongoURI2: 'mongodb+srv://Sootycorn:Mohabgyl12#@sooty-izbgc.mongodb.net/test?retryWrites=true'
+}
